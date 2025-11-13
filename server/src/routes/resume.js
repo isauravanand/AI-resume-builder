b0c6edbaccd8aae691fc1779cc98a2d188d223f1
Saurav Anand <sauravanand1608@gmail.com>
2025-11-13 23:02:12 +0530

    Added Get all Resume Route

--- server/src/routes/resume.js
+++ server/src/routes/resume.js
@@ -5,8 +5,9 @@ const verifyToken = require("../middleware/authMiddleware");
 
 //Resume Routes
 router.post("/resume/create", verifyToken, resumeController.createResume);
+router.get("/resume/:userId", resumeController.getResumeByUser);
+router.get("/resume/my-resume", verifyToken, resumeController.getUserResumes);
 router.put("/resume/:id", verifyToken, resumeController.updateResume);
 router.delete("/resume/:id", verifyToken, resumeController.deleteResume);
-router.get("/:userId", resumeController.getResumeByUser);
 
 module.exports=router;
