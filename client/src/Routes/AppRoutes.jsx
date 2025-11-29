02ceedd1fb195bce4a79156568891501775382fd
Saurav Anand <sauravanand1608@gmail.com>
2025-11-29 12:30:04 +0530

    Removed Bakground and Dashboard Route

--- client/src/Routes/AppRoutes.jsx
+++ client/src/Routes/AppRoutes.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes ,Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import VerifyUser from "../pages/VerifyUser";
-import Dashboard from "../pages/Dashboard";
 import CreateResume from "../pages/CreateResume";
 import GenerateAiResume from "../pages/GenerateAiResume";
 import ResumePreview from "../pages/ResumePreview";
@@ -24,7 +23,6 @@ const AppRoutes = () => {
                     <Route path="/user/register" element={<Register/>}/>
                     <Route path="/user/login" element={<Login/>}/>
                     <Route path="/user/verifyUser" element={<VerifyUser/>}/>
-                    <Route path="/dashboard" element={<Dashboard/>}/>
                     <Route path="/create-resume" element={<CreateResume/>}/>
                     <Route path="/generate-ai-resume/:resumeId/:template" element={<GenerateAiResume/>}/>
                     <Route path="/resume-preview/:resumeId" element={<ResumePreview />} />
